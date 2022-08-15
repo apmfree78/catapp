9b568bc5379f41d54421f9865f634e1db553d001
apmfree78 <amit@profitswami.com>
2022-08-15 14:47:44 -0400

    loaded random cat photo on mount

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios, { AxiosResponse } from 'axios';
+// these will later be set to env variables
+const API_KEY = 'b08e14e2-e1c9-41b9-8ce6-cb76f5ca851f';
+const CAT_URL = 'https://api.thecatapi.com/v1';
 
 function App() {
+  const [cats, setCats] = useState('');
+
+  const getCats = async () => {
+
+    // grab random cat photo
+    const response: void | AxiosResponse = await axios.get(`${CAT_URL}/images/search`)
+      .catch(err => console.error(err));
+
+    if (!response) return;
+
+    // extracting data
+    const { data } = response;
+    const [catributes] = data;
+    console.log(catributes)
+
+    // setting state
+    setCats(catributes.url);
+  }
+
+
+
+  // load random cat image on load
+  useEffect(() => {
+    getCats();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={cats} />
     </div>
   );
 }
