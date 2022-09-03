8108d2f1f3a03dc9b80762793245ae90a9b4766a
apmfree78 <amit@profitswami.com>
2022-09-02 21:48:49 -0400

    added button to load new cat pic and some basic formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,69 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import axios, { AxiosResponse } from 'axios';
-import { CAT_URL } from './env.js'
+
+interface CatTributes {
+  id: string;
+  url: string;
+}
+
+const CAT_URL = 'https://api.thecatapi.com/v1';
 
 function App() {
-  const [cats, setCats] = useState('');
+  const [cats, setCats] = useState<CatTributes>({ id: '', url: '' });
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState('');
 
+  // get random cat image from API
   const getCats = async () => {
+    // loading state begins
+    setLoading(true);
+    setError('');
 
     // grab random cat photo
-    const response: void | AxiosResponse = await axios.get(`${CAT_URL}/images/search`)
-      .catch(err => console.error(err));
+    const response: void | AxiosResponse = await axios
+      .get(`${CAT_URL}/images/search`)
+      .catch((err) => {
+        // set error state
+        setError(err.message);
+        setLoading(false);
+        console.error(err);
+      });
 
-    if (!response) return;
+    // loading done
+    setLoading(false);
+
+    if (!response) {
+      setError('no cat images found! sorry!');
+      return;
+    }
 
     // extracting data
     const { data } = response;
-    const [catributes] = data;
-    console.log(catributes)
+    const [{ url, id }] = data;
+    console.log({ url, id });
 
     // setting state
-    setCats(catributes.url);
-  }
-
-
+    setCats({ url, id });
+  };
 
   // load random cat image on load
   useEffect(() => {
     getCats();
-  }, [])
+  }, []);
 
+  // return jsx showing cat picture and image id
   return (
-    <div className="App">
-      <img src={cats} />
+    <div className='App'>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {cats && (
+        <div style={{ margin: '15px' }}>
+          <img style={{ height: '50vmin' }} src={cats.url} />
+          <p>image ID: {cats.id}</p>
+        </div>
+      )}
+      <button onClick={getCats}>Get Cute Cat Pic</button>
     </div>
   );
 }
