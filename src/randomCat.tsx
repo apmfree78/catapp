2bffadaa8faf1510acd8f1e47483041c1d0c2d0b
apmfree78 <amit@profitswami.com>
2022-10-23 14:25:59 -0400

    setup react query hooks to get random cat pick

--- src/randomCat.tsx
+++ src/randomCat.tsx
@@ -1,77 +1,52 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import axios, { AxiosResponse } from 'axios';
-import { useQuery } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query';
 
 interface CatTributes {
   id: string;
   url: string;
+  width: number;
+  height: number;
 }
 
 const CAT_URL = 'https://api.thecatapi.com/v1';
 
+export async function getCats() {
+  const { data } = await axios.get(CAT_URL);
+  return data;
+}
+
+export function useCats() {
+  const query = useQuery<CatTributes, Error>(['catPics'], () => getCats());
+  return query;
+}
+
 function RandomCat() {
-  const [cats, setCats] = useState<CatTributes>({ id: '', url: '' });
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState('');
+  // const [cats, setCats] = useState<CatTributes>({ id: '', url: '' });
+  // const [loading, setLoading] = useState<boolean>(false);
+  // const [error, setError] = useState('');
 
-  // get random cat image from API
-  const getCats = async () => {
-    // loading state begins
-    setLoading(true);
-    setError('');
+  const { data, isLoading, isError, error } = useCats();
 
-    // grab random cat photo
-    const response: void | AxiosResponse = await axios
-      .get(`${CAT_URL}/images/search`)
-      .catch((err) => {
-        // set error state
-        setError(err.message);
-        setLoading(false);
-        console.error(err);
-      });
+  // if (!data) return <div></div>;
+  console.log(data);
 
-    // loading done
-    setLoading(false);
-
-    if (!response) {
-      setError('no cat images found! sorry!');
-      return;
-    }
-
-    // extracting data
-    const { data } = response;
-    const [{ url, id }] = data;
-    console.log({ url, id });
-
-    // setting state
-    setCats({ url, id });
-  };
-
-  // load random cat image on load
-  useEffect(() => {
-    getCats();
-  }, []);
+  const { url, id } = data || {};
 
   // return jsx showing cat picture and button
   // to load next cat picture
   return (
     <div className='App'>
-      {loading && <p>Loading...</p>}
-      {error && <p data-testid='error'>{error}</p>}
-      {cats.id !== '' && (
+      {isLoading && <p>Loading...</p>}
+      {isError && <p data-testid='error'>'Error'</p>}
+      {id !== '' && (
         <div style={{ margin: '15px' }}>
-          <img
-            style={{ height: '50vmin' }}
-            src={cats.url}
-            alt='cute cat photo'
-          />
+          <img style={{ height: '50vmin' }} src={url} alt='cute cat photo' />
           {/* <p>image ID: {cats.id}</p> */}
         </div>
       )}
-      <button disabled={loading} onClick={getCats}>
-        Get Cute Cat Pic
-      </button>
+      <button disabled={isLoading}>Get Cute Cat Pic</button>
     </div>
   );
 }
